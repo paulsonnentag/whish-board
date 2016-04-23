238de9b90f550aa492c6ac6c39314525df440afd
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-04-23 11:19:16 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+require('../style/app.scss');
+
+export class App extends Component {
+  render() {
+    return (
+      <h1>Hello World !</h1>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    },{
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'autoprefixer',  'sass']
+    },{
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+      ]
+    }]
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, "./style")]
+  }
+};
